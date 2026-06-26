e3a3d29f78f055fffda37867f3a2e39bf834171e
FabricioQs <iefc.kevin.quishpe@gmail.com>
2026-06-25 19:27:10 -0500

    Parte 2: Variables y constantes de posición implementadas; comida reubicada en la esquina inferior derecha

--- cazando.js
+++ cazando.js
@@ -1,15 +1,34 @@
 let canvas = document.getElementById("areaJuego");
 let ctx = canvas.getContext("2d");
 
+const ANCHO_GATO = 50;   
+const ALTO_GATO = 50;    
+const ANCHO_COMIDA = 25; 
+const ALTO_COMIDA = 25;
 
-function graficarGato() {
-    
-    const anchoGato = 50;
-    const altoGato = 50;
-    
-    const x = (canvas.width / 2) - (anchoGato / 2); 
-    const y = (canvas.height / 2) - (altoGato / 2);
+let gatoX = 0;   
+let gatoY = 0;   
+let comidaX = 0; 
+let comidaY = 0;
 
+function iniciarJuego() {
+   
+    gatoX = (canvas.width / 2) - (ANCHO_GATO / 2); 
+    gatoY = (canvas.height / 2) - (ALTO_GATO / 2);
+    
+    comidaX = canvas.width - ANCHO_COMIDA;  
+    comidaY = canvas.height - ALTO_COMIDA;
+
+    graficarGato();
+    graficarComida();
+}
+
+function graficarGato() { 
     ctx.fillStyle = "blue";
-    ctx.fillRect(x, y, anchoGato, altoGato);
+    ctx.fillRect(gatoX, gatoY, ANCHO_GATO, ALTO_GATO);
+}
+
+function graficarComida() {
+    ctx.fillStyle = "red"; 
+    ctx.fillRect(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA);
 }
